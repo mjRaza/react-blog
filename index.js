da75a1a039d755a508df396ced2ea7708a489812
mjRaza <jaffarrazadar@gmail.com>
2020-05-01 02:44:02 +0500

    added environment and connected mongoDB

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./model/user");
+const config = require("./config/key");
 mongoose
-  .connect(
-    "mongodb+srv://jaffar:123123123@react-blog-erahs.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => {
     console.log("db is connected");
   })
   .catch((e) => {
     console.log("error:", error);
   });
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.send("hogaya");
+});
 
 app.listen(port, () => console.log(`Example app listening on port port!`));
